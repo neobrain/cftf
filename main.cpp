f2915802a90b6a54b2b0689377f53adae431ee4c
Tony Wasserka <neobrainx@gmail.com>
2018-05-20 19:40:49 +0200

    Reintroduce hack for reference purposes

--- main.cpp
+++ main.cpp
@@ -11,6 +11,16 @@
 #include <iostream>
 #include <string_view>
 
+#ifdef __linux__
+namespace llvm {
+/**
+ * http://lists.llvm.org/pipermail/llvm-dev/2017-January/109621.html
+ * We can't rebuild llvm, but we can define symbol missed in llvm build.
+ */
+//int DisableABIBreakingChecks = 1;
+}
+#endif
+
 namespace ct = clang::tooling;
 
 namespace cftf {
