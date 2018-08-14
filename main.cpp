c1cef1ddca9a81d48910c8b67720ee987c9fc2d3
Tony Wasserka <neobrainx@gmail.com>
2018-08-14 17:20:59 +0200

    Make the program act as a preprocessor and trigger a proper compilation.
    The backend compiler is specified using CFTF_FRONTEND_CXX. All command line arguments will be forwarded to this compiler.

--- main.cpp
+++ main.cpp
@@ -9,9 +9,11 @@
 #include <clang/Tooling/Tooling.h>
 #include <clang/Rewrite/Core/Rewriter.h>
 #include <llvm/Support/CommandLine.h>
+#include "llvm/Support/Error.h"
 
 #include <iostream>
 #include <memory>
+#include <numeric>
 #include <string_view>
 
 #ifdef __linux__
@@ -206,10 +208,121 @@ private:
 } // namespace cftf
 
 int main(int argc, const char* argv[]){
-    ct::CommonOptionsParser options_parser(argc, argv, cftf::tool_category);
-    ct::ClangTool tool(options_parser.getCompilations(), options_parser.getSourcePathList());
+    using namespace std::string_literals;
+
+    // TODO: Write cftf output to this file!
+    const char temp_output_filename[] = "cftf_temp";
+
+    // TODO: Strip CFTF-specific options from argument list
+    // TODO: Add options specific to the CFTF pass to argument list
+
+    // Indexes into argv, referring to detected input filenames
+    std::vector<size_t> input_filename_arg_indices;
+
+    // Indexes into known command line arguments
+    std::vector<size_t> input_indexes;
+
+    input_filename_arg_indices.push_back(0); // executable name
+    for (size_t arg_idx = 1; arg_idx < static_cast<size_t>(argc); ++arg_idx) {
+        auto arg = argv[arg_idx];
+
+        auto is_cpp_file = [](const char* str) -> bool {
+            // TODO: This is a very incomplete heuristic:
+            //       * Not everybody uses .cpp for C++ files
+            //       * The source language can be overridden by user flags
+            auto len = std::strlen(str);
+            return (len > 3) && (0 == std::strcmp(str + len - 4, ".cpp"));
+        };
+
+        if (std::strcmp(arg, "-") == 0) {
+            // stdin. TODO.
+            std::cerr << "Using stdin as input not handled yet" << std::endl;
+            std::exit(1);
+        } else if (arg[0] == '-') {
+            // This argument is some sort of flag.
+            if (arg[1] == 'D') {
+                // Preprocessor define
+                input_indexes.push_back(arg_idx);
+                if (arg[2] == ' ') {
+                    if (arg_idx + 1 == static_cast<size_t>(argc)) {
+                        std::cerr << "Invalid input: Expected symbol after \"-D\", got end of command line" << std::endl;
+                        std::exit(1);
+                    }
+                    // Include the actual definition, too.
+                    // Note that if a value is provided, it's provided via "=VALUE", i.e. it cannot be space-separated.
+                    input_indexes.push_back(++arg_idx);
+                }
+            } else {
+                std::cerr << "Ignoring command line option \"" << arg << "\"" << std::endl;
+            }
+        } else if (is_cpp_file(arg)) {
+            // TODO: Does this catch all inputs? Is there a way to specify inputs via non-positional command line arguments?
+            input_filename_arg_indices.push_back(arg_idx);
+        }
+    }
+
+    // Restricted command line that only includes all input files
+    std::string internal_command_line =
+        std::accumulate(input_indexes.begin(), input_indexes.end(), ""s,
+            [argv](const std::string& prev, size_t next_idx) {
+                return prev + " " + argv[next_idx];
+            }
+        );
+    std::vector<const char*> internal_argv;
+    std::transform(input_filename_arg_indices.begin(), input_filename_arg_indices.end(), std::back_inserter(internal_argv), [argv](size_t idx) { return argv[idx]; });
+    // TODO: Escape "'" within arguments
+    std::string extra_arg = std::accumulate(input_indexes.begin(), input_indexes.end(), "-extra-arg='"s,
+                                            [&argv](std::string& cur, size_t next_idx) { return cur + " " + argv[next_idx]; });
+    extra_arg += '\'';
+    internal_argv.push_back(extra_arg.c_str());
+    int internal_argc = internal_argv.size();
+
+    std::cerr << "Internal command line: \"";
+    std::copy(internal_argv.begin(), internal_argv.end(), std::ostream_iterator<const char*>(std::cerr, " "));
+    std::cerr << "\"" << std::endl;
+
+    auto options_parser = ct::CommonOptionsParser::create(internal_argc, internal_argv.data(), cftf::tool_category, llvm::cl::ZeroOrMore);
+    if (!options_parser) {
+        llvm::handleErrors(options_parser.takeError(), [](const llvm::ErrorInfoBase& err_info) { llvm::errs() << err_info.message() << '\n'; });
+        std::exit(1);
+    }
+
+    if (options_parser->getSourcePathList().size() > 1) {
+        // TODO: Support multiple input files
+        std::cerr << "Processing multiple files is not supported, yet" << std::endl;
+        std::exit(1);
+    }
+
+    ct::ClangTool tool(options_parser->getCompilations(), options_parser->getSourcePathList());
 
     int result = tool.run(ct::newFrontendActionFactory<cftf::FrontendAction>().get());
+
+    const char* frontend_command = std::getenv("CFTF_FRONTEND_CXX");
+    if (!frontend_command || frontend_command[0] == 0) {
+        std::cerr << "Error: CFTF_FRONTEND_CXX not set" << std::endl;
+        exit(1);
+    }
+
+    // Replace original input filenames with temp_output_filename
+    std::string modified_cmdline = frontend_command;
+    for (size_t arg_idx = 0; arg_idx < static_cast<size_t>(argc); ++arg_idx) {
+        auto arg = argv[arg_idx];
+
+        if (arg_idx != 0 && input_filename_arg_indices.end() != std::find(input_filename_arg_indices.begin(), input_filename_arg_indices.end(), arg_idx)) {
+            // Positional argument; this is an input file, probably
+            std::cerr << "Replacing presumable input argument \"" << arg << "\" with \"" << temp_output_filename << "\"" << std::endl;
+            // TODO: Wrap filename in quotes!
+            modified_cmdline += " "s + temp_output_filename;
+        } else {
+            // Other argument; just copy this to the new command line
+            // TODO: Wrap arguments in quotes or escape them!
+            modified_cmdline += " "s + arg;
+        }
+    }
+
+    // Trigger proper compilation
+    std::cerr << "Invoking \"" << modified_cmdline << "\"" << std::endl;
+    std::system(modified_cmdline.c_str());
 
     return result;
 }
